b0ec445c340167c12fff46de9c5d67f1b3f7c54f
Andrii Polonskyi <apolonskyi@alteryx.com>
2025-07-01 17:12:10 +0200

    pre-lesson-19

--- lesson-19/cypress.config.js
+++ lesson-19/cypress.config.js
@@ -1,8 +1,23 @@
-const { defineConfig } = require("cypress");
+import { defineConfig } from "cypress";
 
-module.exports = defineConfig({
+export default defineConfig({
+  retries: {
+    runMode: process.env.CI ? 2 : 0,
+    openMode: 0
+  },
+  video: true,
+  viewportHeight: 1080,
+  viewportWidth: 1920,
   e2e: {
+    specPattern: 'cypress/e2e/**/*.test.{js,jsx,ts,tsx}',
+    baseUrl: 'https://example.cypress.io',
     setupNodeEvents(on, config) {
+      on('task', {
+        log(message) {
+          console.log(message)
+          return null
+        },
+      })
       // implement node event listeners here
     },
   },

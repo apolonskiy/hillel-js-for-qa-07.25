89a278361d19e48a0274fbab5ce38a5146040d09
Andrii Polonskyi <apolonskyi@alteryx.com>
2025-07-11 20:05:51 +0200

    lesson-22

---
+++ lesson-22/tests/example-2.spec.js
@@ -0,0 +1,40 @@
+// @ts-check
+import { test, expect } from '@playwright/test';
+
+test.describe('Longer test', () => {
+
+  test.beforeEach(async({ page, context, browser, request }) => {
+    console.log(page.url());
+    console.log('SHOW_THIS_VAR', process.env.SHOW_THIS_VAR);
+  })
+
+  test.only('Click Github', async ({ page, context, browser, request }) => {
+    await page.pause()
+    const context2 = await browser.newContext();
+    await page.goto('https://playwright.dev/');
+    let pages = context.pages();
+    console.log(pages.length);
+
+
+    const githubPagePromise = context.waitForEvent('page');
+  
+    // Click the get started link.
+    await page.locator('[class*="navbar"] div[class="theme-layout-navbar-right navbar__items navbar__items--right"] a[aria-label="GitHub repository"]').click();
+    const githubPage = await githubPagePromise;
+
+    await expect(githubPage.url()).toContain('github.com');
+    await expect(page.url()).not.toContain('github.com');
+    // Expects page to have a heading with the name of Installation.
+
+    await page.waitForTimeout(2000);
+    pages = context.pages();
+    await expect(pages[1].url()).toContain('github.com');
+    console.log(pages.length);
+
+    await context.newPage();
+    await page.waitForTimeout(2000);
+
+    pages = context.pages();
+    console.log(pages.length);
+  });
+})
